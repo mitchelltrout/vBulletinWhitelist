9a60f3f12fd7e3bd5f05e91ac77b75fab41af94b
Mitchell <mitchelltrout@laughingcactus.net>
2011-03-15 22:52:56 -0400

    added more config settings

--- src/com/mitchelltrout/vBulletinWhitelist/VbwPlayerListener.java
+++ src/com/mitchelltrout/vBulletinWhitelist/VbwPlayerListener.java
@@ -16,17 +16,16 @@ public class VbwPlayerListener extends PlayerListener {
     }
 
     public void onPlayerLogin(PlayerLoginEvent event)
-    {
-    	
-    	
-    	
+    {    	
     	String playerName = event.getPlayer().getName();
     	
-    	if(!vBulletinWhitelist.isMember(playerName))
+    	if(vBulletinWhitelist.isMember(playerName) == 0)
     	{
     		event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "You are not a registered user of " + vBulletinWhitelist.getForumPath());
     	}
-    	
-    	
+    	else if(vBulletinWhitelist.isMember(playerName) == 2)
+    	{
+    		event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Error while verifing username at " + vBulletinWhitelist.getForumPath());
+    	}
     }
 }
--- src/com/mitchelltrout/vBulletinWhitelist/vBulletinWhitelist.java
+++ src/com/mitchelltrout/vBulletinWhitelist/vBulletinWhitelist.java
@@ -32,6 +32,9 @@ public class vBulletinWhitelist extends JavaPlugin {
     private final VbwPlayerListener playerListener = new VbwPlayerListener(this);
     public static PluginDescriptionFile pdfFile;
     public static String forumpath = "";
+    public static String forumsys = "";
+    public static String profilefield = "";
+    public static Boolean strictmode = false;
 
     public void onDisable() {
     	pdfFile = this.getDescription();
@@ -41,47 +44,15 @@ public class vBulletinWhitelist extends JavaPlugin {
     public void onEnable() {
     	
     	pdfFile = this.getDescription();
-    	BufferedWriter Writer = null;
     	
     	if(!new File("plugins/vBulletinWhitelist/vBulletinWhitelist.properties").exists())
         {
-            try
-            {
-            	new File("plugins/vBulletinWhitelist/").mkdir();
-            	new File("plugins/vBulletinWhitelist/vBulletinWhitelist.properties").createNewFile();
-                Writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("plugins/vBulletinWhitelist/vBulletinWhitelist.properties")));
-                Writer.write("# The path to your forums directory. Please include the trailing slash.\r\nforumpath=http://www.yourwebsite.com/path/to/forum/");
-                Writer.flush();
-                Writer.close();
-                Log.log(Level.INFO, "[" + pdfFile.getName() + "] vBulletinWhitelist.properties created");
-            }
-            catch(Exception e)
-            {
-                Log.log(Level.WARNING, "[" + pdfFile.getName() + "] Exception while creating vBulletinWhitelist.properties");
-                e.printStackTrace();
-            }
+            writeConfig();
         }
     	
     	try
         {
-            Scanner scan = new Scanner(new FileInputStream("plugins/vBulletinWhitelist/vBulletinWhitelist.properties"));
-            String input, var, val;
-            while(scan.hasNextLine())
-            {
-                input = scan.nextLine();
-
-                if(input.startsWith("#")) continue;
-
-                var = input.substring(0, input.indexOf("=")).toLowerCase();
-                val = input.substring(input.indexOf("=") + 1);
-
-                if(var.equals("forumpath"))
-                {
-                    forumpath = val;
-                }
-            }
             
-            scan.close();
         }
         catch(Exception e)
         {
@@ -96,15 +67,15 @@ public class vBulletinWhitelist extends JavaPlugin {
         Log.log(Level.INFO, "[" + pdfFile.getName() + "] version " + pdfFile.getVersion() + " is enabled!");
     }
     
-    public static boolean isMember(String playername)
+    public static int isMember(String playername)
     {
-    	boolean member = false;
+    	int member = 0; //0=false, 1=true, 2=error
     	
     	Log.log(Level.INFO, "[" + pdfFile.getName() + "] " + playername + " is attempting to join server...");
     	
     	try {
     		
-    		URL address = new URL(forumpath + "mcwhitelist.php?user=" + playername);
+    		URL address = new URL(forumpath + "mcwhitelist.php?fsys=" + forumsys + "&pf=" + profilefield + "&user=" + playername);
 			URLConnection yc = address.openConnection();
 			BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
 			String inputLine;
@@ -112,32 +83,125 @@ public class vBulletinWhitelist extends JavaPlugin {
 				
 				if(inputLine.equals("true"))
 				{
-					member = true;
+					Log.log(Level.INFO, "[" + pdfFile.getName() + "] " + playername + " is registered.");
+					member = 1;
+				}
+				else if(inputLine.equals("false"))
+				{
+					Log.log(Level.INFO, "[" + pdfFile.getName() + "] " + playername + " is not registered. DENIED!");
+					member = 0;
 				}
 				else
 				{
-					member = false;
+					member = 2; //anything else means error in php
+					Log.log(Level.WARNING, "[" + pdfFile.getName() + "] Webserver gave unknown response.");
 				}
 
 			}
 			in.close();
 		} catch (Exception e) {
-			//System.out.print("vBulletinWhitelist: Could not connect to feverclan.com");
 			Log.log(Level.WARNING, "[" + pdfFile.getName() + "] Exception while trying to read " + forumpath + "mcwhitelist.php");
+			
+			if(strictmode)
+				member = 2;
+			else
+				member = 1;
+			
 			e.printStackTrace();
 		}
 
+    	return member;
+    }
+    
+    public static void writeConfig()
+    {
+    	BufferedWriter Writer = null;
     	
-    	if(member)// || playername.equals("quarkman")
-    	{
-    		Log.log(Level.INFO, "[" + pdfFile.getName() + "] " + playername + " is registered.");
-    		return true;
-    	}
-    	else
-    	{
-    		Log.log(Level.INFO, "[" + pdfFile.getName() + "] " + playername + " is not registered. DENIED!");
-    		return false;
-    	}
+    	try
+        {
+    		new File("plugins/vBulletinWhitelist/").mkdir();
+        	new File("plugins/vBulletinWhitelist/vBulletinWhitelist.properties").createNewFile();
+            Writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("plugins/vBulletinWhitelist/vBulletinWhitelist.properties")));
+            Writer.write("# The path to your forums directory. Please include the trailing slash.\r\nforumpath=http://www.yourwebsite.com/path/to/forum/\r\n");
+            Writer.write("# Forum System. vBulletin=1, MyBB=2, phpBB=3\r\nforumsys=0\r\n");
+            Writer.write("# Database profile field name.\r\nprofilefield=minecraftUsernameFieldID\r\n");
+            Writer.write("# If strictmode is false plugin will allow connections when it can not connect to forum.\r\nstrictmode=true\r\n");
+            Writer.flush();
+            Writer.close();
+            Log.log(Level.INFO, "[" + pdfFile.getName() + "] vBulletinWhitelist.properties created");
+        }
+        catch(Exception e)
+        {
+            Log.log(Level.WARNING, "[" + pdfFile.getName() + "] Exception while creating vBulletinWhitelist.properties");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void readConfig()
+    {
+    	int varcount = 0;
+    	
+    	try
+        {
+    		Scanner scan = new Scanner(new FileInputStream("plugins/vBulletinWhitelist/vBulletinWhitelist.properties"));
+            String input, var, val;
+            while(scan.hasNextLine())
+            {
+                input = scan.nextLine();
+
+                if(input.startsWith("#")) continue;
+
+                var = input.substring(0, input.indexOf("=")).toLowerCase();
+                val = input.substring(input.indexOf("=") + 1);
+
+                if(var.equals("forumpath"))
+                {
+                    forumpath = val;
+                    varcount++;
+                }               
+                else if(var.equals("forumsys"))
+                {
+                    forumsys = val;
+                    varcount++;
+                }                
+                else if(var.equals("profilefield"))
+                {
+                    profilefield = val;
+                    varcount++;
+                }
+                else if(var.equals("strictmode"))
+                {
+                    if(val.equals("false"))
+                    	strictmode = false;
+                    else
+                    	strictmode = true;
+                    
+                    varcount++;
+                }
+            }
+            
+            scan.close();
+        }
+        catch(Exception e)
+        {
+            Log.log(Level.WARNING, "[" + pdfFile.getName() + "] Exception while reading vBulletinWhitelist.properties");
+            e.printStackTrace();
+        }
+        
+        if(varcount < 4) //not all config settings were read. the plugin will not work properly.
+        {
+        	Log.log(Level.WARNING, "[" + pdfFile.getName() + "] MISSING CONFIG SETTINGS. WRITING NEW CONFIG FILE.");
+        	if(new File("plugins/vBulletinWhitelist/vBulletinWhitelist.properties").delete() && new File("plugins/vBulletinWhitelist/").delete())
+        	{
+        		writeConfig();
+        		Log.log(Level.WARNING, "[" + pdfFile.getName() + "] YOU MUST EDIT THE NEW CONFIG SETTINGS AND RESTART SERVER FOR PLUGIN TO WORK!");
+        	}
+        	else
+        	{
+        		Log.log(Level.WARNING, "[" + pdfFile.getName() + "] COULD NOT DELETE OLD CONFIG.");
+        	}
+        	
+        }
     }
     
     public static String getForumPath()
